59544773084435f347d2be1b0b124597047f7e26
liudw <ldw4033@163.com>
2014-11-20 15:10:14 +0800

    add HelloGit

---
+++ gitTest/src/gitTest/HelloGit.java
@@ -0,0 +1,18 @@
+/*   
+ * Copyright (c) 2013-2023 LanYoung tech co., LTD. All Rights Reserved.   
+ *   
+ * This software is the confidential and proprietary information of   
+ * LanYoung tech. You shall not disclose such Confidential Information   
+ * and shall use it only in accordance with the terms of the agreements   
+ * you entered into with LanYoung tech.   
+ *   
+ */     
+package gitTest;    
+    
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+
+}
+  
